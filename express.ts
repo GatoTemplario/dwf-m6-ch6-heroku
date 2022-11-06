adc438197bfc61837653452a7142feac0a779912
Eleri <n.erik.jak@gmail.com>
2022-11-06 15:58:09 -0300

    cambio en object.hasown

--- express.ts
+++ express.ts
@@ -3,6 +3,8 @@ import express from "express";
 const app = express()
 const port = process.env.PORT || 3000;
 
+app.use(express.static('dist'))
+
 app.listen(port, () => {
     console.log(`Example app listening ${port}`);
 })
