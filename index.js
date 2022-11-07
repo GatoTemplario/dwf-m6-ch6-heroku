87786489e23e71839da72bf5a3e91cee5a6f4ca0
Eleri <n.erik.jak@gmail.com>
2022-11-07 18:06:46 -0300

    commit 68

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var router_1 = require("./src/router");
-require("./src/pages/chat");
-require("./src/pages/welcome");
-(function () {
-    router_1.router;
-})();
